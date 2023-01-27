c3e6cb31c9d7dad642f039751dbf1c7883ae8708
Ellen Bennett <ellenbennett@Ellens-MacBook-Air.local>
2023-01-26 17:38:14 -0700

    passing lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function superbowlWin (record) {
+    let result = undefined;
+    for (let item of record) {
+        if (item.result === 'W') {
+            result = item.year;
+            break;
+        }; 
+
+    };
+
+    return result;
+}; 
+
+let winningYear = record.find(superbowlWin);
